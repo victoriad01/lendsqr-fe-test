3ac2b3b43f8b768eceb8f26f95334f16e5cc2c5d
victoriad01 <94958284+victoriad01@users.noreply.github.com>
2023-05-20 12:13:46 +0100

    fixed: something css issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.scss'
-import './index.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Login from './Pages/Login/Login'
 import Users from './Pages/Users/Users'
